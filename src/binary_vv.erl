a3e9f5df239c0a2909eca852873972b9e9dfd9cb
Russell Brown <russell.brown@mac.com>
2015-11-15 07:12:24 +0000

    Create a version vector that doesn't need serilisation

---
+++ src/binary_vv.erl
@@ -0,0 +1,104 @@
+-module(binary_vv).
+
+-compile(export_all).
+
+-define(INT, 64).
+
+%% If you always use N bytes for an actor ID, and then have 1 64-bit
+%% integer as the counter
+new(Size) ->
+    <<Size:32/integer>>.
+
+increment(Actor, <<Len:32/integer, Rest/binary>>) ->
+    VV = increment(Len, Actor, Rest, <<>>, 0),
+    <<Len:32/integer, VV/binary>>.
+
+increment(Len, Actor, <<>>, <<>>, 0) ->
+    <<Actor:Len/binary,
+              1:?INT/integer>>;
+increment(Len, Actor, Bin, Preq, LenPreq) ->
+    case Bin of
+        <<>> ->
+            <<Preq:LenPreq/binary, Actor:Len/binary, 1:?INT/integer>>;     
+        <<Actor:Len/binary, Counter:?INT/integer, Rest/binary>> ->
+            Counter2 = Counter+1,
+            <<Preq:LenPreq/binary, Actor:Len/binary, Counter2:?INT/integer, Rest/binary>>;
+        <<Other:Len/binary, OtherC:?INT/integer, OtherRest/binary>> ->
+            increment(Len, Actor, OtherRest, <<Other:Len/binary, OtherC:?INT/integer, Preq/binary>>, LenPreq+Len+(?INT div 8))
+    end.
+
+to_vv(<<Size:32/integer, Rest/binary>>) ->
+    to_vv(Size, Rest).
+
+to_vv(_Size, <<>>) ->
+    [];
+to_vv(Size, Bin) ->
+     <<Actor:Size/binary, Counter:?INT/integer, Rest/binary>> = Bin,
+    [{Actor, Counter} | to_vv(Size, Rest)].
+
+from_vv([{Actor1, _} | _Tail]=VV) ->
+    Size = byte_size(Actor1),
+    Actors = lists:foldl(fun({Actor, Cntr}, Acc) ->
+                                 <<Actor:Size/binary, Cntr:?INT/integer, Acc/binary>>
+                         end,
+                         <<>>,
+                         VV),
+    <<Size:32/integer, Actors/binary>>.
+
+descends(<<Size:32/integer, RestA/binary>>, <<Size:32/integer, RestB/binary>>) ->
+    descends(Size, RestA, RestB).
+
+descends(_Size, _, <<>>) ->
+    %% all clocks descend the empty clock and themselves
+    true;
+descends(_Size, B, B) ->
+    true;
+descends(_Size, A, B) when byte_size(A) < byte_size(B) ->
+    %% A cannot descend B if it is smaller than B. To descend it must
+    %% cover all the same history at least, shorter means fewer actors
+    false;
+descends(Size, A, B) ->
+    <<ActorB:Size/binary, CntrB:?INT/integer, RestB/binary>> = B,
+    case take_entry(Size, ActorB, A) of
+        {<<>>, 0} ->
+            false;
+        {RestA, CntrA} ->
+            (CntrA >= CntrB) andalso descends(Size, RestA, RestB)
+    end.
+
+take_entry(_Size, _Actor, <<>>) ->
+    {<<>>, 0};
+take_entry(Size, Actor, Bin) ->
+    case Bin of
+        <<Actor:Size/binary, Counter:?INT/integer, Rest/binary>> ->
+            {Rest, Counter};
+        <<Other:Size/binary, OtherC:?INT/integer, OtherRest/binary>> ->
+            {Rest, Counter} = take_entry(Size, Actor, OtherRest),
+            {<<Other:Size/binary, OtherC:?INT/integer, Rest/binary>>, Counter}
+    end.
+
+dominates(A, B) ->
+    descends(A, B) andalso not descends(B, A).
+
+merge([<<Size:32/integer, _/binary>>=First | Clocks]) ->
+    merge(Size, Clocks, First).
+
+merge(_Size, [], Mergedest) ->
+    Mergedest;
+merge(Size, [Clock | Clocks], Mergedest) ->
+    merge(Size, Clocks, merge(Size, Clock, Mergedest));
+merge(Size, <<Size:32/integer, ClockA/binary>>,
+      <<Size:32/integer, ClockB/binary>>) ->
+    Merged = merge_clocks(Size, ClockA, ClockB, <<>>),
+    <<Size:32/integer, Merged/binary>>.
+
+merge_clocks(_Size, <<>>, B, Acc) ->
+    <<B/binary, Acc/binary>>;
+merge_clocks(_Size, A, <<>>, Acc) ->
+    <<A/binary, Acc/binary>>;
+merge_clocks(Size, A, B, Acc) ->
+    <<ActorA:Size/binary, CounterA:?INT/integer, RestA/binary>> = A,
+    {RestB, CounterB} =  take_entry(Size, ActorA, B),
+    Counter = max(CounterB, CounterA),
+    merge_clocks(Size, RestA, RestB,
+                 <<ActorA:Size/binary, Counter:?INT/integer, Acc/binary>>).
---
+++ test/vv_bm.erl
@@ -0,0 +1,67 @@
+-module(vv_bm).
+
+-compile(export_all).
+
+increment() ->
+    Actors = [crypto:rand_bytes(24) || _ <- lists:seq(1, 20)],
+    Events = 100000,
+    increment(Actors, Events, binary_vv:new(24), term_to_binary(riak_dt_vclock:fresh()), {[], []}).
+
+increment(_Actors, 0, _BVV, _VC, Times) ->
+    stats(Times);
+increment(Actors, Events, BVV, VC, Times) ->
+    Actor = random_actor(Actors),
+    {BVVTime, NewBVV} = timer:tc(binary_vv, increment, [Actor, BVV]),
+    F = fun() -> VC0 = binary_to_term(VC), VC1 = riak_dt_vclock:increment(Actor, VC0), term_to_binary(VC1) end,
+    {VCTime, NewVC} = timer:tc(F),%%riak_dt_vclock, increment, [Actor, VC]),
+    increment(Actors, Events - 1, NewBVV, NewVC, update_times(BVVTime, VCTime, Times)).
+    
+    
+random_actor([Actor]) ->
+    Actor;
+random_actor(Actors) ->
+    N = crypto:rand_uniform(1, length(Actors)),
+    lists:nth(N, Actors).
+
+update_times(BVVTime, VCTime, {BVVTimes, VCTimes}) ->
+    {[BVVTime | BVVTimes], [VCTime | VCTimes]}.
+
+stats({BVVTimes, VCTimes}) ->
+    {
+      {bvv, stats(BVVTimes)},
+      {vc, stats(VCTimes)}
+    };
+stats(Times) ->
+    Min = lists:min(Times),
+    Max = lists:max(Times),
+    Avg = average(Times),
+    Sorted = lists:sort(Times),
+    Percentiles = [percentile(Sorted, P) || P <- [0.25, 0.75, 0.90, 0.99]],
+    [{quantiles, Percentiles},
+     {min, Min},
+     {max, Max},
+     {avg, Avg}, 
+     {raw, Sorted}].
+
+percentile(Sorted, P) ->
+    Index = P * length(Sorted),
+    percentile(trunc(Index), ceil(Index), Sorted).
+
+percentile(Index, Index, Sorted) ->
+    %% Whole number, av of nth + nth+1
+    average([lists:nth(Index, Sorted) , lists:nth(Index+1, Sorted)]);
+percentile(_, Index, Sorted) ->
+    lists:nth(Index, Sorted).
+
+average(L) ->
+    Sum = lists:sum(L),
+    Sum / length(L).
+    
+ceil(X) ->
+    T = erlang:trunc(X),
+        case (X - T) of
+        Neg when Neg < 0 ->
+                T;
+            Pos when Pos > 0 -> T + 1;
+            _ -> T
+    end.
